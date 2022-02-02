70a6a620fce7167d5d776bba7392ae2fdbfac501
Egor Gorokhov <egorgorokhovmn@gmail.com>
2022-02-03 00:46:31 +0300

    Add initial support for the AOF persistence

--- src/cache.go
+++ src/cache.go
@@ -1,15 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"os"
+)
 
 type Cache struct {
 	_cache map[string]string
+	logger *log.Logger
 }
 
-func (c *Cache) initCache() {
+func (c *Cache) init() {
 	c._cache = make(map[string]string)
+	c.logger = log.New(os.Stdout, "cache-logger", log.LstdFlags)
 
-	fmt.Println("The cache created!")
+	c.logger.Println("The cache created!")
 }
 
 func (c *Cache) set(key, value string) {
--- src/server.go
+++ src/server.go
@@ -2,9 +2,12 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net"
 	"os"
 	"strings"
+	"sync"
+	"time"
 )
 
 const (
@@ -16,33 +19,38 @@ const (
 // Global cache to store key-value pairs
 var cache Cache
 
+// Aof to handle persistence
+var aof Aof
+
 func main() {
+	logger := log.New(os.Stdout, "server-logger", log.LstdFlags)
 	listener, err := net.Listen(CONN_TYPE, HOST+":"+PORT)
 	defer listener.Close()
 
 	if err != nil {
-		fmt.Println("Can't start listener on port", PORT, ".", err)
+		logger.Println("Can't start listener on port", PORT, ".", err)
 		os.Exit(1)
 	}
 
-	cache.initCache()
+	// Initialize needed structs
+	cache.init()
+	aof.init()
 
-	fmt.Println("Server started on port:", PORT)
+	logger.Println("Server started on port:", PORT)
 
 	for {
 		connection, err := listener.Accept()
-
 		if err != nil {
-			fmt.Println("Can't accept a connection:", err)
+			logger.Println("Can't accept a connection:", err)
 		}
-
 		handleConnection(connection)
 	}
 }
 
 func handleConnection(connection net.Conn) {
 	defer connection.Close()
-	buffer := make([]byte, 2*1024)
+	// TODO: Make it possible to receive queries larger than 1024 bytes
+	buffer := make([]byte, 1024)
 
 	_, err := connection.Read(buffer)
 
@@ -51,28 +59,29 @@ func handleConnection(connection net.Conn) {
 		return
 	}
 
-	command := strings.Trim(string(buffer), "\n")
-	splitCommand := strings.Split(command, " ")
+	query := strings.Trim(string(buffer), "\n")
+	queryArray := strings.Split(query, " ")
 
-	response := handleCommand(splitCommand)
+	response := processQuery(queryArray)
 
 	connection.Write([]byte(response))
 }
 
 // returns respond for the given command
-func handleCommand(arguments []string) string {
+func processQuery(query []string) string {
 
-	command := arguments[0]
+	command := query[0]
 
 	switch command {
 	case "set":
-		key, value := arguments[1], arguments[2]
+		key, value := query[1], query[2]
 		fmt.Println("SET for", key, "->", value)
 		cache.set(key, value)
+		aof.appendLog(strings.Join(query, " "))
 		return key + "->" + value + " is set!"
 
 	case "get":
-		key := arguments[1]
+		key := query[1]
 		fmt.Println("GET for", key)
 		value, ok := cache.get(key)
 		if ok == false {
@@ -81,7 +90,61 @@ func handleCommand(arguments []string) string {
 		} else {
 			return value
 		}
+
 	default:
 		return "Unknown command!"
+	}
+}
+
+func startPersistence() {
+	for {
+		time.Sleep(time.Second)
+		aof.flushAof()
+	}
+}
+
+// Aof stores info needed to implement AOF persistence
+type Aof struct {
+	path    string
+	aofBuf  string
+	aofLock *sync.Mutex
+	logger  *log.Logger
+}
+
+func (aof *Aof) init() {
+	aof.path = "/tmp/bootleg-mmdb-persistence/aof.pers"
+	aof.aofBuf = ""
+	aof.aofLock = new(sync.Mutex)
+	aof.logger = log.New(os.Stdout, "aof-logger", log.LstdFlags)
+
+	aof.logger.Println("Aof is ready!")
+}
+
+func (aof *Aof) appendLog(query string) {
+	aof.aofLock.Lock()
+	aof.aofBuf = aof.aofBuf + query + "\n"
+	aof.aofLock.Unlock()
+}
+
+func (aof *Aof) flushAof() {
+
+	// TODO: Somehow handle these file related errors
+	aof.aofLock.Lock()
+	tempAofBuf := aof.aofBuf
+	aof.aofBuf = ""
+	aof.aofLock.Unlock()
+
+	aofFile, err := os.OpenFile(aof.path, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	if _, err := aofFile.Write([]byte(tempAofBuf)); err != nil {
+		aofFile.Close()
+		log.Panic(err)
+	}
+
+	if err := aofFile.Close(); err != nil {
+		log.Panic(err)
 	}
 }
